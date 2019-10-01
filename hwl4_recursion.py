e7febd61873d236a8279654fcd8277640bd2bd51
ivanvaskevich <i_vaskevich@adamantium.net>
2019-10-01 22:59:25 +0300

    hwl4

--- hwl4_recursion.py
+++ hwl4_recursion.py
@@ -4,7 +4,8 @@
 #
 # rec('hello')('world')
 
-# recursion
+##############
+# recursion shows all type of elements
 # a = [
 #     {
 #         (1, 2): [
@@ -35,7 +36,46 @@
 # rec(a)
 
 
+###########
+# rec() with lst in arg
 
+# a = [
+#     {
+#         (1, 2): [
+#             {'b': 1, 'c': 2},
+#             'hello world'
+#         ],
+#         'b': {'d': 3}
+#     },
+#     4,
+#     5,
+#     ['hello', 'again']
+# ]
+#
+# def rec(a, lst=None):
+#     if lst is None:
+#         lst = []
+#     if isinstance(a, (list, tuple)):
+#         # print('a: list')
+#         for i in a:
+#             rec(i, lst)
+#     elif isinstance(a, dict):
+#         # print('a: dict')
+#         for i, j in a.items():
+#             rec(i, lst)
+#             rec(j, lst)
+#     # elif isinstance(a, str):
+#     #     print('a: str')
+#     else:
+#         # print('a:{}'.format(type(a)))
+#         lst.append(a)
+#     return lst
+# print(rec(a))
+
+
+
+##################################
+# optimization - rec() without lst in arg
 a = [
     {
         (1, 2): [
@@ -49,22 +89,22 @@ a = [
     ['hello', 'again']
 ]
 
-def rec(a, lst=None):
-    if lst is None:
-        lst = []
+lst = []
+def add(a):
+    lst.append(a)
+    return
+
+def rec(a):
     if isinstance(a, (list, tuple)):
-        # print('a: list')
         for i in a:
-            rec(i, lst)
+            rec(i)
     elif isinstance(a, dict):
-        # print('a: dict')
         for i, j in a.items():
-            rec(i, lst)
-            rec(j, lst)
-    # elif isinstance(a, str):
-    #     print('a: str')
+            rec(i)
+            rec(j)
+    elif isinstance(a, str):
+        add(a)
     else:
-        # print('a:{}'.format(type(a)))
-        lst.append(a)
-    return lst
-print(rec(a))
+        add(a)
+rec(a)
+print(lst)
